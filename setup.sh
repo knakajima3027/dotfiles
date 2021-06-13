50288182f79cf5415dfcac590760907fd1f57503
kNakajima3027 <kamihate1012@gmail.com>
2021-06-14 00:07:20 +0900

    Change zinitのインストールを別ファイルに分離

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
+# alias
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.inputrc ~/.inputrc
 mkdir ~/.vim/colors
+ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/vim/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
 ln -sf ~/dotfiles/vim/tender.vim/colors/tender.vim ~/.vim/colors/tender.vim
 
-ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+# install bash-completion
+brew install bash-completion
 
-# zinit
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
-# vim-plug
+# install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
