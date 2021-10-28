ed71aa3efda4f269938092d246430beaecaead05
knakajima3027 <kamihate1012@gmail.com>
2021-10-28 22:44:40 +0900

    整理

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+DOT_DIRECTORY="${HOME}/dotfiles"
+
 # シンボリックリンクを貼る
 # シンボリックリンクを貼りたくないファイルをブラックリスト形式で書く
 for file in .??*
 do
     [[ "$file" == ".git" ]] && continue
     [[ "$file" == ".DS_Store" ]] && continue
+    [[ "$file" == ".gitmodules" ]] && continue
 
     echo "$file"
-    ln -svf $file ~/
+    ln -svf ${DOT_DIRECTORY}/$file ${HOME}/${file}
 done
+
+# vimのカラースキーマのシンボリックリンク
+ln -sf ~/dotfiles/vim/tender.vim/colors/tender.vim ~/.vim/colors/tender.vim
